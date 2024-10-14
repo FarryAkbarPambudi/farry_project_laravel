c3ddd601086854d05114049a7cda0d5bad40380e
FarryAkbarPambudi <541231072@student.smktelkom-pwt.sch.id>
2024-10-14 15:38:57 +0700

    Farry

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //ini menuju ke halaman welcome.blade.php di folder resource/view
+});
+
+Route::get('/index', function () {
+    return view('index'); //ini menuju ke halaman index.blade.php di folder resource/view
+});
+
+Route::get('/', function () {
+    return view('login'); //ini menuju ke halaman login.blade.php di folder resource/view
 });
